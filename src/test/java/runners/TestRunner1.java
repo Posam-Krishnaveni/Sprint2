b4fdbbb3ce6e693d275c4e15da32c777da057668
Posam-Krishnaveni <psrkrohm@gmail.com>
2020-12-08 16:46:43 +0530

    MyProject

--- src/test/java/runners/TestRunner1.java
+++ src/test/java/runners/TestRunner1.java
@@ -1,11 +1,15 @@
 package runners; 
+
 import cucumber.api.CucumberOptions;
+
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 @CucumberOptions(features="Feature999", glue={"stepDefinitions"},
+
 plugin = {"json:target/cucumber.json"},
+
 tags={"@Add_to_cart,@Successful_login"})
-//tags={"@Successful_login,@Unsuccessful_login"}
-//tags={"@Successful_Registration,@first_name_field_validation,@last_name_field_validation,@email_field_validation,@password_field_validation,@confirm_password_field_validation"}
+
 public class TestRunner1 extends AbstractTestNGCucumberTests {
 
 }
